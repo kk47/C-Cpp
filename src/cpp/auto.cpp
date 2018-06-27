5d0e51f538475fe558c0cf01e1a88ae2485cb871
kk47 <kk47yx@gmail.com>
2018-06-27 16:20:01 +0800

    - add new files

---
+++ src/cpp/auto.cpp
@@ -0,0 +1,26 @@
+#include <initializer_list>
+#include <vector>
+#include <iostream>
+
+class MagicFoo {
+public:
+    std::vector<int> vec;
+    MagicFoo(std::initializer_list<int> list) {
+        // 从 C++11 起, 使用 auto 关键字进行类型推导
+        for (auto it = list.begin(); it != list.end(); ++it) {
+            vec.push_back(*it);
+        }
+    }
+};
+int main() {
+    auto i = 5;              // i 被推导为 int
+    auto arr = new auto(10);
+
+    MagicFoo magicFoo = {1, 2, 3, 4, 5};
+    std::cout << "magicFoo: ";
+    for (auto it = magicFoo.vec.begin(); it != magicFoo.vec.end(); ++it) {
+        std::cout << *it << ", ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
